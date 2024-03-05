66cc17449dd6dcf7431212c4c93e2fcdf098431d
Максим Ледовский <santech_montage@mail.ru>
2024-03-05 22:17:22 +0300

    made base for the first point of the task P.S. programming realization of the first point must be done then

--- Lab_2/Lab_2.cpp
+++ Lab_2/Lab_2.cpp
@@ -1,5 +1,11 @@
 ﻿#include <iostream>
 #include <Windows.h>
+#include <filesystem>
+#include <errno.h>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,8 +14,88 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	unsigned short int action;
+
 	do
 	{
+		cout << "Выберите действие:\n"
+			<< "1. Создать файл с исходными данными\n"
+			<< "2. Загрузить исходные данные\n"
+			<< "3. Вывести исходные данные\n"
+			<< "4. Обработать данные\n"
+			<< "5. Завершить работу с программой" << endl;
+		cin >> action;
+
+		if (cin.fail())
+		{
+			cout << "Введено некорректное значение!\n"
+				<< "Код ошибки:" << errno << endl;
+			perror("Системное сообщение об ошибке: ");
+			_set_errno(0);
+			continue;
+		}
+		else if (action == 0 || action > 5)
+		{
+			cout << "Введено некорректное значение!\n";
+			continue;
+		}
+
+		switch (action == 5)
+		{
+		case 5:
+			cout << "Работа программы завершена!" << endl;
+			exit(0);
+			break;
+		case 1:
+			size_t size;
+			do
+			{
+				cout << "Введите количество элементов, которые вы хотите записать: " << endl;
+				cin >> size;
+			} while (cin.fail());
+			
+			double* arr = new double;
+			try
+			{
+				arr = new double[size];
+			}
+			catch (bad_alloc)
+			{
+				cout << "Ошибка выделения памяти!\n"
+					<< "Код ошибки: " << errno << endl;
+				perror("Системное сообщение об ошибке: ");
+				_set_errno(0);
+				continue;
+			}
+
+			string value;
+			double num;
+			
+			cout << "Поочерёдно вводите значения:\n";
+			for (size_t i = 0; i < size; i++)
+			{
+				cin >> value;
+				replace(value.begin(), value.end(), ',', '.');
+				istringstream iss(value);
+				iss >> num;
+				if (iss.fail())
+				{
+					iss.clear();
+					int position = iss.tellg();
+					char c = iss.peek();
+					cout << "Файл содержит некорректное значение: \"" << value << "\"\n"
+						<< "Некорректный символ последовательности: \'" << c << "\'\n"
+						<< "Абсолютная позиция некорректного символа последовательности: " << position + 1 << '\n';
+					i--;
+					cout << "Введите значение повторно: ";
+				}
+			}
+
+			string filename;
+			cout << "Введите двоичное имя файла: " << endl;
+			cin >> filename;
+		}
+
 
 	} while (true);
 
