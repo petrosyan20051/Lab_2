a5aa262c579d88fbc77f537a9f993d304685fbc7
Максим Ледовский <santech_montage@mail.ru>
2024-03-05 21:32:38 +0300

    Добавьте файлы проекта.

---
+++ Lab_2/Lab_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	do
+	{
+
+	} while (true);
+
+	return 0;
+}
